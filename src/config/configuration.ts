bba58c2443cac280d0ddd1014cbb0480d6ec12e4
caioaugustoo <caioamfr@gmail.com>
2023-06-09 00:03:00 -0300

    refactor: remove useless folder

--- src/config/configuration.ts
+++
@@ -1,9 +0,0 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 8000,
-  discord: {
-    token: process.env.DISCORD_TOKEN,
-  },
-  database: {
-    url: process.env.DATABASE_URL,
-  },
-});

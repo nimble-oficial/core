efc55838dc01fb76e35202b746b2e5f6cbace8bb
caioaugustoo <caioamfr@gmail.com>
2023-06-10 19:56:03 -0300

    refactor: change allowed channel schema

--- src/modules/commands/dto/update-command.dto.ts
+++ src/modules/commands/dto/update-command.dto.ts
@@ -15,24 +15,33 @@ export const updateCommandDtoSchema = z
         message: "Name cannot exceed 20 characters.",
       }),
     description: z
-      .string({
-        required_error: "Description is required.",
-      })
-      .min(1, {
-        message: "Description is required.",
-      })
+      .string()
       .max(100, {
         message: "Description cannot exceed 100 characters.",
-      }),
+      })
+      .optional(),
     enabled: z.boolean().default(true),
     allowedChannel: z
-      .string()
+      .object({
+        id: z
+          .string()
+          .optional()
+          .default(DEFAULT_OPTION_VALUES.allowedChannel.id),
+        name: z
+          .string()
+          .optional()
+          .default(DEFAULT_OPTION_VALUES.allowedChannel.name),
+      })
       .optional()
       .default(DEFAULT_OPTION_VALUES.allowedChannel),
-    allowedRole: z
-      .string()
-      .optional()
-      .default(DEFAULT_OPTION_VALUES.allowedRole),
+
+    allowedRole: z.object({
+      id: z.string().optional().default(DEFAULT_OPTION_VALUES.allowedRole.id),
+      name: z
+        .string()
+        .optional()
+        .default(DEFAULT_OPTION_VALUES.allowedRole.name),
+    }),
     commandId: z.string(),
     guildId: z.string(),
   })
